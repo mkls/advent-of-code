433397569b7ecafb678737f103a416c21e91ca94
Miklós Antal <miklos.antal@emarsys.com>
2021-12-01 06:22:05 +0100

    first puzzle

--- src/01.js
+++ src/01.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const lines = require('fs').readFileSync('src/01.txt', 'utf-8').split('\n');
+const lines = require('fs').readFileSync('src/01-a.txt', 'utf-8').split('\n').map(Number);
 
-console.log(lines);
+let increases = 0;
+for (let i = 3; i < lines.length; i++) {
+  const current = lines[i] + lines[i - 1] + lines[i - 2];
+  const prev = lines[i - 1] + lines[i - 2] + lines[i - 3];
+  if (current > prev) {
+    increases++;
+  }
+}
+console.log(increases);
