034d65b4b5d516ef9df43358bff055c757b06413
Miklós Antal <miklos.antal@emarsys.com>
2021-12-03 06:45:01 +0100

    03 part 2

--- src/03/solution.js
+++ src/03/solution.js
@@ -5,32 +5,41 @@ const { groupBy } = require('lodash');
 const rawLines = require('fs')
   .readFileSync(__dirname + '/actual.txt', 'utf-8')
   .split('\n');
-// const rawLines = require('fs').readFileSync(__dirname + '/actual.txt', 'utf-8').split('\n');
+
+const lines = rawLines.map(line => line.split('').map(Number));
 
 const numFromBinary = bits =>
   bits.reverse().reduce((accu, bit, index) => accu + bit * 2 ** index, 0);
 
-const lines = rawLines.map(line => line.split('').map(Number));
+const filterByCommonness = (binaries, filter, index = 0) => {
+  if (binaries.length === 1) {
+    return binaries;
+  }
 
-const groupedByIndex = [];
-lines.forEach(bits =>
-  bits.forEach((bit, i) => {
-    groupedByIndex[i] = (groupedByIndex[i] || []).concat(bit);
-  })
-);
+  const groupedByIndex = [];
+  binaries.forEach(bits =>
+    bits.forEach((bit, i) => {
+      groupedByIndex[i] = (groupedByIndex[i] || []).concat(bit);
+    })
+  );
 
-const counts = groupedByIndex.map(group => {
-  const zeros = group.filter(d => d === 0).length;
-  const ones = group.filter(d => d === 1).length;
-  return { ones, zeros };
-});
+  const counts = groupedByIndex.map(group => {
+    const zeros = group.filter(d => d === 0).length;
+    const ones = group.filter(d => d === 1).length;
+    return { ones, zeros };
+  });
 
-const mostCommons = counts.map(d => (d.ones > d.zeros ? 1 : 0));
-const leastCommons = counts.map(d => (d.ones < d.zeros ? 1 : 0));
+  const bitToKeep = filter(counts[index])
+  const kept = binaries.filter(num => num[index] === bitToKeep);
+  // console.log({ counts, bitToKeep, kept });
 
-console.log({ mostCommons });
+  return filterByCommonness(kept, filter, index + 1);
+};
 
-const gamma = numFromBinary(mostCommons);
-const epsilon = numFromBinary(leastCommons);
 
-console.log({ gamma, epsilon, result: gamma * epsilon });
+let [oxigen] = filterByCommonness(lines, d => d.ones >= d.zeros ? 1 : 0);
+let [co2] = filterByCommonness(lines, d => d.zeros <= d.ones ? 0 : 1);
+
+oxigen = numFromBinary(oxigen);
+co2 = numFromBinary(co2);
+console.log({ co2, oxigen, result: co2 * oxigen });
