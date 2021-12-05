f5f4afbed673b758ef0d4ff132b2a954623089e7
Miklós Antal <miklos.antal@emarsys.com>
2021-12-05 11:40:50 +0100

    2020 01

---
+++ 2020/01/solution-1.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('\n\n ---- Script start ----- \n\n');
+
+const numbers = require('fs')
+  .readFileSync(__dirname + '/actual.txt', 'utf-8')
+  .split('\n')
+  .map(Number);
+
+const result = numbers.filter(n => numbers.some(m => n + m === 2020));
+
+console.log(result);
+console.log(result[0] * result[1]);
