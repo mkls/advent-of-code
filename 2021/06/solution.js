0daf85e3c0a11f270c661067fadfd7e6e6bb41c1
Miklós Antal <miklos.antal@emarsys.com>
2021-12-06 06:34:10 +0100

    06 initial solution

--- 2021/06/solution.js
+++ 2021/06/solution.js
@@ -1,7 +1,35 @@
 'use strict';
 
-const lines = require('fs')
-  .readFileSync(__dirname + '/example.txt', 'utf-8')
-  .split('\n');
+const { groupBy, mapValues, toPairs, sumBy, map } = require('lodash');
 
-console.log(lines);
+let fishes = require('fs')
+  .readFileSync(__dirname + '/actual.txt', 'utf-8')
+  .split(',')
+  .map(Number);
+
+let counts = toPairs(
+  mapValues(
+    groupBy(fishes, a => a),
+    value => value.length
+  )
+).map(([days, count]) => ({ day: +days, count }));
+
+
+for (let i = 1; i <= 256; i++) {
+  const newCounts = counts.flatMap(({ day, count }) => {
+    if (day === 0) {
+      return [
+        { day: 6, count },
+        { day: 8, count }
+      ];
+    }
+    return [{ day: day - 1, count }];
+  });
+  const grouped = mapValues(groupBy(newCounts, 'day'), v => ({
+    day: v[0].day,
+    count: sumBy(v, 'count')
+  }));
+  counts = map(grouped, a => a);
+}
+
+console.log(sumBy(counts, 'count'));
