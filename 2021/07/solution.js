9157bfe5abc95f9cd57db7a6320b2644de610d71
Miklós Antal <miklos.antal@emarsys.com>
2021-12-07 06:30:50 +0100

    07 performance improvement

--- 2021/07/solution.js
+++ 2021/07/solution.js
@@ -3,20 +3,18 @@
 const _ = require('lodash'); // eslint-disable-line no-unused-vars
 
 const positions = require('fs')
-  .readFileSync(__dirname + '/example.txt', 'utf-8')
+  .readFileSync(__dirname + '/actual.txt', 'utf-8')
   .split(',')
   .map(Number);
 
 const min = Math.min(...positions);
 const max = Math.max(...positions);
 
-const cost = (a, b) => {
-  const diff = Math.abs(a - b);
-  return _.sum(_.range(1, diff + 1));
-};
+const cost = _.memoize(diff => _.sum(_.range(1, diff + 1)));
 
-const fuelToPosition = _.range(min, max + 1).map(position => {
-  return positions.map(p => cost(p, position)).reduce(_.add);
+const fuelToPosition = _.range(min, max + 1).map(target => {
+  const costsToHere = positions.map(p => cost(Math.abs(target - p)));
+  return _.sum(costsToHere);
 });
 
 const minCost = Math.min(...fuelToPosition);
