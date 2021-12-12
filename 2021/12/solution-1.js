d7b17275ddafc7f2f76090e0c6f0a734e0427e0b
Miklós Antal <miklos.antal@emarsys.com>
2021-12-12 06:49:16 +0100

    12

---
+++ 2021/12/solution-1.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const _ = require('lodash'); // eslint-disable-line no-unused-vars
+
+const connections = require('fs')
+  .readFileSync(__dirname + '/example.txt', 'utf-8')
+  .split('\n')
+  .map(line => line.split('-'));
+
+const caves = {};
+_.uniq(connections.flat()).forEach(id => {
+  caves[id] = { isBig: /[A-Z]/.test(id) };
+});
+
+connections.forEach(([s, e]) => {
+  caves[s].paths = (caves[s].paths || []).concat(e);
+  caves[e].paths = (caves[e].paths || []).concat(s);
+});
+
+const getPaths = (id, pathSoFar = []) => {
+  if (id === 'end') {
+    return [{ isValid: true, pathSoFar }];
+  }
+
+  const goals = caves[id].paths.filter(
+    c => c !== 'start' && (!pathSoFar.includes(c) || caves[c].isBig)
+  );
+  if (goals.length === 0) {
+    return [{ isValid: false }];
+  }
+  return goals.flatMap(g => getPaths(g, [...pathSoFar, g])).filter(r => r.isValid);
+};
+
+
+const res = getPaths('start', ['start']).map(r => r.pathSoFar);
+console.log(res);
+console.log(res.length);
