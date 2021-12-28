19ac26c30831f3c868f406a2140efd133a470423
Miklos Antal <miklos.antal@emarsys.com>
2021-12-28 10:58:50 +0100

    refact

--- 2020/14/solution.js
+++ 2020/14/solution.js
@@ -3,23 +3,24 @@
 const _ = require('lodash'); // eslint-disable-line no-unused-vars
 
 exports.main = input => {
-  const finalState = input.split('\n').reduce(
-    (state, line) => {
-      if (line.startsWith('mask')) {
-        return { ...state, mask: line.slice(-36) };
-      } else {
-        const [, addr, value] = line.match(/mem\[(\d+)\] = (\d+)$/);
-        return { ...state, memory: { ...state.memory, [addr]: applyMask(+value, state.mask) } };
-      }
-    },
-    { mask: null, memory: {} }
-  );
+  let mask;
+  const memory = {};
 
-  return _.sum(Object.values(finalState.memory));
-};
+  for (const line of input.split('\n')) {
+    if (line.startsWith('mask')) {
+      mask = line.slice(-36);
+    } else {
+      const [, addr, value] = line.match(/mem\[(\d+)\] = (\d+)$/);
+      memory[addr] = Number(value)
+        .toString(2)
+        .padStart(36, '0')
+        .split('')
+        .map((c, i) => (mask[i] === 'X' ? c : mask[i]))
+        .reverse()
+        .map((bit, i) => Number(bit) * 2 ** i)
+        .reduce((a, b) => a + b);
+    }
+  }
 
-const applyMask = (value, mask) => {
-  const valueAsBinary = value.toString(2).padStart(36, '0');
-  const masked = valueAsBinary.split('').map((c, i) => (mask[i] === 'X' ? c : mask[i]));
-  return _.sum(masked.reverse().map((bit, i) => Number(bit) * 2 ** i));
+  return _.sum(Object.values(memory));
 };
