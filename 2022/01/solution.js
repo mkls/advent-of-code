4116e7e3e0061892b9ee2189a1072a38db0deb63
Miklos Antal <miklos.antal@emarsys.com>
2022-12-01 07:49:12 +0100

    2022 01 2

--- 2022/01/solution.js
+++ 2022/01/solution.js
@@ -4,5 +4,5 @@ const _ = require('lodash'); // eslint-disable-line no-unused-vars
 
 exports.main = input => {
   const caloriesPerElf = input.split('\n\n').map(l => _.sum(l.split('\n').map(Number)));
-  return _.max(caloriesPerElf);
+  return _.sum(caloriesPerElf.sort((a, b) => b - a).slice(0, 3));
 };
