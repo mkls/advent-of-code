57f2b431fc7f11fa77cdf5b4f39ac4b3800f56fd
Miklos Antal <miklos.antal@emarsys.com>
2022-12-02 07:33:22 +0100

    22 02 2

--- 2022/02/solution.js
+++ 2022/02/solution.js
@@ -7,18 +7,22 @@ const scoreForLetter = {
   Y: 2,
   Z: 3
 }
-const wins = ['A Y', 'B Z', 'C X'];
-const draws = ['A X', 'B Y', 'C Z'];
+const shapeScores = {
+  A: { 6: scoreForLetter['Y'], 3: scoreForLetter['X'], 0: scoreForLetter['Z'] },
+  B: { 6: scoreForLetter['Z'], 3: scoreForLetter['Y'], 0: scoreForLetter['X'] },
+  C: { 6: scoreForLetter['X'], 3: scoreForLetter['Z'], 0: scoreForLetter['Y'] }
+}
 
 exports.main = input => {
-  return _.sum(input.split('\n').map(l => {
-    const shapeScore = scoreForLetter[l[2]];
-    return shapeScore + getGameScore(l);
-  }));
+  const points = input.split('\n').map(l => {
+    const score = getGameScore(l);
+    return score + shapeScores[l[0]][score];
+  });
+  return _.sum(points);
 };
 
 const getGameScore = l => {
-  if (wins.includes(l)) return 6;
-  if (draws.includes(l)) return 3;
+  if (l[2] === 'Z') return 6;
+  if (l[2] === 'Y') return 3;
   return 0;
 }
